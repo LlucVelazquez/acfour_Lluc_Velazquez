f6b19a161dd0a8ffc16ce8642856f11b39e7c763
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 10:44:57 +0100

    implementacio c#

--- acfour_Lluc_Velazquez/Program.cs
+++ acfour_Lluc_Velazquez/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const string Msg = "Quants nombres naturals vols?: ";
+        const string DemanarNum = "Introdueix un nombre: ";
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int cuants = 0;
+            try
+            {
+                Console.WriteLine(Msg);
+                cuants = Int16.Parse(Console.ReadLine());
+            }
+            catch (InvalidDataException)
+            {
+
+            }
+            
+            int[] nums = new int[cuants];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(DemanarNum);
+                nums[i] = Int16.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
         }
     }
 }
