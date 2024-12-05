6ebee8c17edd18aed89e75a36d799785ae1f8619
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 09:42:50 +0100

    Add project files.

---
+++ acfour_Lluc_Velazquez/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace acfour_Lluc_Velazquez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
